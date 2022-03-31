c8cb6b1bf75dcb7e74f04ac4d15136f4ca9f8b82
Xuan <97ssps30212@gmail.com>
2022-03-31 13:36:33 +0800

    Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+module.exports = {
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
