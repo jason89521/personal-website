9714dde35ecb4fa1a507cc7b98f523150d6d8939
Xuan <97ssps30212@gmail.com>
2022-04-07 23:43:36 +0800

    Remove test text from header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,20 @@ import type { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import SvgSprite from 'components/SvgSprite';
-import { firestore } from 'firebase/server';
 
 type Props = {
   skills: string[];
-  baz: string;
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const skills = ['html', 'css', 'sass', 'tailwindcss', 'js', 'ts', 'react', 'redux', 'nextjs', 'git'];
-  const snapshot = await firestore.doc('foo/bar').get();
-  const baz: string = snapshot.get('baz');
 
   return {
-    props: { skills, baz },
+    props: { skills },
   };
 };
 
-export default function Home({ skills, baz }: Props) {
+export default function Home({ skills }: Props) {
   return (
     <main>
       <Head>
@@ -30,7 +26,6 @@ export default function Home({ skills, baz }: Props) {
 
       <header className="py-32">
         <h1 className="text-center font-serif text-8xl capitalize 2xl:text-6xl xl:text-4xl">A front-end developer</h1>
-        {baz}
       </header>
 
       <section className="py-4">
