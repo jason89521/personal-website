946f7345cc4a6fc50e655c914bb2b4c40133720c
Xuan <97ssps30212@gmail.com>
2022-04-07 23:32:37 +0800

    Add replace method at FIREBASE_PRIVATE_KEY

--- firebase/server.ts
+++ firebase/server.ts
@@ -7,7 +7,7 @@ if (getApps().length <= 0) {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
   });
 }
