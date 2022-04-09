46d8b4e19ce41d25a2201a35da614c557f605cd9
Xuan <97ssps30212@gmail.com>
2022-04-09 12:05:24 +0800

    Remove title link at blog page
    Now user can only click on the 'Read More' link to enter a post.

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,9 +1,9 @@
 import type { GetStaticProps } from 'next';
 import Link from 'next/link';
+import Head from 'next/head';
 
 import { getPostPreviews } from 'lib/post';
 import Markdown from 'components/Markdown';
-import Head from 'next/head';
 
 type Props = {
   previews: ReturnType<typeof getPostPreviews>;
@@ -20,22 +20,7 @@ export default function Blog({ previews }: Props) {
         const { excerpt, id } = preview;
         return (
           <li key={id} className="mx-auto mb-10 max-w-xl">
-            <Markdown
-              className="prose max-w-none py-10 dark:prose-invert xl:py-5"
-              components={{
-                h1: ({ node, children, ...rest }) => {
-                  return (
-                    <h1 {...rest}>
-                      <Link href={`/blog/${id}`} passHref>
-                        <a className="font-extrabold no-underline hover:underline">{children}</a>
-                      </Link>
-                    </h1>
-                  );
-                },
-              }}
-            >
-              {excerpt}
-            </Markdown>
+            <Markdown className="prose max-w-none py-10 dark:prose-invert xl:py-5">{excerpt}</Markdown>
 
             <Link href={`/blog/${id}`} passHref>
               <a className="text-xl font-semibold underline transition-all hover:tracking-widest xl:text-lg">
