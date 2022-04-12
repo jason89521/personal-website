68f4e14f97baea56e726e5ccd3e9c2551bbce09b
Xuan <97ssps30212@gmail.com>
2022-04-12 11:09:11 +0800

    Add icon-eye

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 
 import avatar from 'public/avatar.jpg';
+import SvgSprite from './SvgSprite';
 
 type Props = {
   title: string;
@@ -11,10 +12,14 @@ const PostHeader = ({ title }: Props) => {
   return (
     <header>
       <h1>{title}</h1>
-      <div>
+      <div className="flex items-center gap-4">
         <span className="flex items-center gap-2 font-semibold">
           <Image src={avatar} alt="avatar" className="rounded-full" width={32} height={32} />
           Xuan
+        </span>
+        <span className="flex items-center gap-2">
+          <SvgSprite category="icon" symbolId="eye" className="h-4 w-4 dark:fill-slate-300" />
+          1234
         </span>
       </div>
     </header>
