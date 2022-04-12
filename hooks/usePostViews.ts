3df847e878a3fbf850755c77a6ccab48f02a0f31
Xuan <97ssps30212@gmail.com>
2022-04-12 21:00:30 +0800

    Try to use useSWR to prevent too many updating on views

--- hooks/usePostViews.ts
+++ hooks/usePostViews.ts
@@ -3,11 +3,12 @@ import useSWR from 'swr';
 import createFetcher from 'lib/createFetcher';
 
 export default function usePostViews(id: string, shouldUpdate?: boolean) {
-  const { data, error } = useSWR<{ views: number }>(`/api/post/${id}`, createFetcher());
+  const config = shouldUpdate ? { method: 'POST' } : undefined;
+  const { data, error } = useSWR<{ views: number }>(`/api/post/${id}`, createFetcher(config));
 
   useEffect(() => {
     if (process.env.NODE_ENV === 'development' || !shouldUpdate) return;
-    createFetcher({ method: 'POST' })(`/api/post/${id}`);
+    // createFetcher({ method: 'POST' })(`/api/post/${id}`);
   }, [id, shouldUpdate]);
 
   return data?.views || 0;
