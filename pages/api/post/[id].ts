853bab65b9faf7f0ba62a63aeff410124f60202b
Xuan <97ssps30212@gmail.com>
2022-04-24 13:41:10 +0800

    Reject creating a doc of non-exist post

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,23 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { firestore } from 'firebase/server';
+import { getAllPosts } from 'lib/post';
 
 type Data = {
   views: number;
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+type Error = {
+  message: string;
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data | Error>) {
   const method = (req.method || 'GET').toUpperCase();
-  const { id } = req.query;
-  const docRef = firestore.collection('posts').doc(id.toString());
+  const id = req.query.id as string;
+  const docRef = firestore.collection('posts').doc(id);
   const doc = await docRef.get();
+  const views = doc.get('views') || 0;
   if (!doc.exists) {
-    doc.ref.create({ views: 1 });
-    res.status(200).json({ views: 1 });
-    return;
+    const allPosts = getAllPosts();
+    if (!allPosts.includes(id)) {
+      res.status(400).json({ message: 'This post does not exist.' });
+      return;
+    }
+    await docRef.create({ views });
   }
 
-  const views = doc.get('views');
   if (method === 'POST') {
     docRef.update({ views: views + 1 });
   }
