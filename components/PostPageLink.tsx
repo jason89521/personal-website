18d140bbea6ae4be877a41094a9cfeb2c563cf56
Xuan <97ssps30212@gmail.com>
2022-04-25 15:12:35 +0800

    Add page link at the every post's bottom

---
+++ components/PostPageLink.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+type Props = {
+  metadata: PostMetadata;
+  isOlder?: boolean;
+};
+
+const PostPageLink = ({ metadata, isOlder }: Props) => {
+  return (
+    <Link href={`/blog/${metadata.id}`}>
+      <a className="block h-full rounded-md border px-4 py-2 hover:border-primary">
+        <div className="mb-2 font-medium capitalize">{isOlder ? 'older' : 'newer'} post</div>
+        <div className="font-bold text-primary">{metadata.title}</div>
+      </a>
+    </Link>
+  );
+};
+
+export default PostPageLink;
--- pages/blog/[post].tsx
+++ pages/blog/[post].tsx
@@ -1,34 +1,42 @@
 import type { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import { getPostData, getAllPosts } from 'lib/post';
 import Markdown from 'components/Markdown';
 import PostHeader from 'components/PostHeader';
-import usePostViews from 'hooks/usePostViews';
+import PageLink from 'components/PostPageLink';
 
 type Props = {
   metadata: PostMetadata;
   content: string;
+  olderPost: PostMetadata | false;
+  newerPost: PostMetadata | false;
 };
 
 type Query = {
   post: string;
 };
 
-export default function Post({ metadata, content }: Props) {
+export default function Post({ metadata, content, olderPost, newerPost }: Props) {
   const { title, description, id } = metadata;
 
   return (
-    <main className="px-5">
+    <main className="mx-auto max-w-post px-5 pt-10 pb-20">
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
       </Head>
 
-      <article className="prose mx-auto max-w-post pt-10 pb-20 dark:prose-invert">
+      <article className="prose mb-16 max-w-none dark:prose-invert">
         <PostHeader id={id} title={title} shouldUpdateViews />
         <Markdown>{content}</Markdown>
       </article>
+
+      <nav className="flex justify-between gap-10 xl:gap-5">
+        <div className="flex-1">{newerPost && <PageLink metadata={newerPost} />}</div>
+        <div className="flex-1 text-right">{olderPost && <PageLink metadata={olderPost} isOlder />}</div>
+      </nav>
     </main>
   );
 }
@@ -45,10 +53,16 @@ export const getStaticPaths: GetStaticPaths<Query> = async () => {
 export const getStaticProps: GetStaticProps<Props, Query> = async ({ params }) => {
   const post = params!.post;
   const { metadata, content } = getPostData(post);
+  const allPosts = getAllPosts();
+  const index = allPosts.indexOf(post);
+  const olderPost = index > 0 && getPostData(allPosts[index - 1]).metadata;
+  const newerPost = index < allPosts.length - 1 && getPostData(allPosts[index + 1]).metadata;
   return {
     props: {
       metadata,
       content,
+      olderPost,
+      newerPost,
     },
   };
 };
